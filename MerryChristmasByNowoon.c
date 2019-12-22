01a707e54b873d45f21420665e002e9b8180b09e
GodNowoon <59138974+GodNowoon@users.noreply.github.com>
2019-12-22 21:36:25 +0900

    Add files via upload
    christmas_ver_computer_science

---
+++ MerryChristmasByNowoon.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+
+	printf("\n\n");
+	printf("						          *      \n");
+	printf("						         * *     \n");
+	printf("						        * * *    \n");
+	printf("						         * *     \n");
+	printf("						        * * *    \n");
+	printf("						       * * * *   \n");
+	printf("						      * * * * *   \n");
+	printf("						        * * *    \n");
+	printf("						      *  *  * *   \n");
+	printf("						     * * * * * *   \n");
+	printf("						   *  *   *   *  *  \n");
+	printf("						 * * * * * * * * * *  \n");
+	printf("						          *      \n");
+	printf("						          *      \n");
+	printf("						   **************** \n");
+	printf("						   Merry Christmas!\n");
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	return 0;
+}
